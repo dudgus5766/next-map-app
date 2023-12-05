476c48de1eed1d606ab96149331cf85b24286991
dudgus5766 <dudgus5766@gmail.com>
2023-12-05 19:11:06 +0900

    Add: Google Analytics script 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
 import { DefaultSeo } from 'next-seo';
+import Script from 'next/script';
 import { ThemeProvider } from 'styled-components';
 import SEO from '@/seo.config';
 import GlobalStyle from '@/styles/global-style';
@@ -10,7 +11,25 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
+        {/** SEO */}
         <DefaultSeo {...SEO} />
+        {/** GA https://github.com/vercel/next.js/blob/canary/examples/with-google-analytics/pages/_app.js */}
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-293TKKQ748`}
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-293TKKQ748');
+          `,
+          }}
+        />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
